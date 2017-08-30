3671151c53b9f5c45c05de85a57f44caa81fc37d
Yusuf Bektaş <agolho@gmail.com>
2017-08-31 01:16:44 +0300

    Quitter alpha version
    Saves the date if not exist in the Local Storage. Calculates the time
    passed from the given time. Pushes out 2 facts (as of now) to notify if
    certain milestones have been reached.

---
+++ index.js
@@ -0,0 +1,37 @@
+var shitincoming = "",quitDate="";
+var Data = localStorage.getItem("quitter-data");
+console.log(Date());
+$( "#picker" ).submit(function( event ) {
+  var quitDate = $( "input:first" ).val();
+  alert(quitDate);
+  event.preventDefault();
+  if (Data == null){
+    quitDate=0;
+    localStorage.setItem("quitter-data",quitDate);
+  } else {
+    localStorage.setItem("quitter-data",quitDate);
+  }
+});
+
+if (Data != null) {
+  var then = new Date(Data).getTime();
+  var now =  new Date().getTime();
+  var distance = then - now;
+  distance = -distance;
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  $("#counter").html( days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ");
+} else{
+  $("#picker-container").html("<form id='picker' class='form-control' action='index.html' method='post'><div class=''><input type='datetime-local' class='form-control'></div><button type='submit' class='btn btn-large btn-primary' name='button'>Done</button></form>");
+}
+
+if (hours > 0 || minutes > 20) {
+  $("#facts").html("<p><b>20m: </b>Your heart rate have returned to normal.</p>");
+  if (hours > 2 || days > 1) {
+    $("#facts").append("<p><b>2h: </b>Your heart rate and blood pressure have returned to almost completely normal levels, and you can feel the warmth in your fingertips!</p>");
+  }
+}
